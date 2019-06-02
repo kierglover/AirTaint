13791accde88b1eb5a99c6fb6447c1481c2cbcf5
Kieran Glover <kieran@workwithmad.com>
2019-06-02 11:50:01 +0700

    :tada: Initial commit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">Hello World</text>
+    </view>
+</template>
+
+<script>
+export default {
+    data: function() {
+    return {
+      message: "Hello World"
+    };
+  }
+}
+</script>
+
+ 
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
